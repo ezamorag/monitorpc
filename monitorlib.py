61e8226f4e87a17068af03079c31052e88588f0d
Erik Zamora <ezamora1981@gmail.com>
2023-12-28 21:38:29 -0600

    Update monitorlib.py

--- monitorlib.py
+++ monitorlib.py
@@ -6,7 +6,8 @@ import numpy as np
 
 # Future work: 
 #   Include hotkeys  https://pynput.readthedocs.io/en/latest/keyboard.html
-#   Include PC sound
+#   Include PC sound and webcam
+#   Make the same for smartphones
 
 class Monitorpc:
     def __init__(self, Tf):
@@ -20,14 +21,18 @@ class Monitorpc:
         
     # Monitoring
     def start(self):
+        height, width, layers = self.capture_screen().shape
+        video = cv2.VideoWriter('screencast.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 1.0/self.Tp, (width, height))
         print("Monitoring is working ...")
         self.start_time = time.time()
         with mouse.Listener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll) as listener_mouse, \
             keyboard.Listener(on_press=self.on_press) as listener_keyboard:
             while time.time() - self.start_time < self.Tf:
-                self.screenshots.append(self.capture_screen())
+                video.write(self.capture_screen())
                 time.sleep(self.Tp)
-        self.savedata()
+        cv2.destroyAllWindows()
+        video.release()
+        self.savevents()
         print("Monitoring was ended ...")
             
     # Stores mouse events
@@ -50,21 +55,15 @@ class Monitorpc:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         return frame
     
-    def savedata(self): 
-        def writedata(filename, items):
+    def savevents(self): 
+        def writevents(filename, items):
             with open(filename, "w") as file:
                 for item in items:
                     file.write(",".join(str(i) for i in item)+"\n")
                     
-        writedata("mouse_clicks.txt", self.mouse_clicks)
-        writedata("mouse_moves.txt", self.mouse_moves)
-        writedata("mouse_scrolls.txt", self.mouse_scrolls)
-        writedata("keys_pressed.txt", self.keys_pressed)
+        writevents("mouse_clicks.txt", self.mouse_clicks)
+        writevents("mouse_moves.txt", self.mouse_moves)
+        writevents("mouse_scrolls.txt", self.mouse_scrolls)
+        writevents("keys_pressed.txt", self.keys_pressed)
         
-        height, width, layers = self.screenshots[0].shape
-        video = cv2.VideoWriter('screencast.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 1.0/self.Tp, (width, height))
-        for img in self.screenshots:
-            video.write(img)
-        cv2.destroyAllWindows()
-        video.release()
     
